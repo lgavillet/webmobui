63a3e5bb121f9dd7b7bc29d6c70dfc23bef963c0
lgavillet <loris.gavillet@thallium.ch>
2025-03-09 17:08:12 +0100

    Correction favoris

---
+++ Corrections/Cours 7/Favoris/src/elements/song-item.js
@@ -0,0 +1,60 @@
+const playClick = new CustomEvent('play_click')
+const favoriteClick = new CustomEvent('favorite_click')
+
+class SongItem extends HTMLElement {
+  // Définit la liste des attributs qui seront observés et donc appelerons attributeChangedCallback
+  // lorsqu'il y a une modification
+  static observedAttributes = ['favorite']
+
+  // Appelé lorsque que l'on insert l'élément dans le DOM, typiquement au moment de:
+  // songList.appendChild(newElement)
+  connectedCallback() {
+    this.render()
+  }
+
+  // Appelé lorsque que l'on modifie un attribut présent dans observedAttributes, typiquement au moment de:
+  // newElement.setAttribute('favorite', true)
+  // newElement.setAttribute('favorite', false)
+  attributeChangedCallback() {
+    this.render()
+  }
+
+  // Methode "custom" pour faire le rendering. Nom arbitraire
+  render() {
+    // Attenion à bien tester 'true' et non pas true ! Ne pas oublier que les attributs
+    // sont passés comme des chaines de caractères et non des objets
+    const icon = this.getAttribute('favorite') == 'true' ? 'favorite' : 'favorite_border'
+
+    // On agglomère le HTML
+    this.innerHTML = `<a href="#">
+      <div class="list-item-title">${this.getAttribute('title')}</div>
+      <div class="list-item-actions">
+        <button type="button" class="icon-button favorite-button ">
+          <span class="material-icons">${icon}</span>
+        </button>
+        <button type="button" class="icon-button play-button">
+          <span class="material-icons">play_arrow</span>
+        </button>
+      </div>
+    </a>`
+
+    // Lorsque l'on clique sur "play-button", on intercepte l'évenement et on lève un évenement custom
+    // sur SongItem lui-même. Cela permet au javascript qui va utiliser SongItem de ne pas avoir à se préoccuper
+    // de sa structure, ni de re-binder l'événement si entre temps il y a eu un nouveau rendering
+    // Concept de chacun son job!
+    this.querySelector('.play-button').addEventListener('click', (e) => {
+      e.preventDefault()
+      this.dispatchEvent(playClick)
+    })
+
+    // Pareil ici, juste un nom différent
+    this.querySelector('.favorite-button').addEventListener('click', (e) => {
+      e.preventDefault()
+      this.dispatchEvent(favoriteClick)
+    })
+  }
+}
+
+// Déclare le tag du custom element et la classe à utiliser pour le créer dans le DOM
+// Pas besoin d'exporter, juste d'être appelé une fois
+customElements.define('song-item', SongItem)
---
+++ Corrections/Cours 7/Favoris/src/sections/songs.js
@@ -0,0 +1,91 @@
+import { loadSongs, loadSearch } from '../api.js'
+import playSong from './player.js'
+import {getItem, getItems, setItem, removeItem} from '../local-storage.js'
+
+// Récupérer le tag contenant la liste des chansons et le titre de la section
+const songList = document.querySelector('.list')
+const titreList = document.querySelector('#list-section h4')
+
+
+// S'occupe de remplir la liste d'objets "chansons" reçue en paramètres, directement dans le HTML
+// en créant les différents éléments 'song-item' et en leur passant les infos nécessaires à l'affichage
+//
+// Elle se charge également de lier des eventsListeners à nos customs events créés pour l'occasion
+const displaySongArray = (songs) => {
+  // Vider la liste
+  songList.innerHTML = ''
+
+  // Itérer le tableau d'artistes reçus et créer les éléments correspondants
+  songs.forEach((song) => {
+    // Créer l'élément
+    const songItem = document.createElement('song-item')
+
+    songItem.setAttribute('title', song.title)
+    songItem.setAttribute('favorite', !!getItem(song.id))
+
+    // NB: Pourquoi '!!' ? En gros: ça permet de convertir ce qui suit en booléen true/false.
+    // Il y a une valeur ? -> C'est true
+    // Il n'y a pas de valeur (null, undefined, false,...) ? -> C'est false
+    // Autrement, getItem retourne soit l'objet qu'il retrouve, soit un null, ce qui n'est pas
+    // très joli d'un point de vue code... voilà :)
+
+    // Lorsque l'on clique sur play
+    songItem.addEventListener('play_click', () => {
+      playSong(song, songs)
+    })
+
+    // Lorsque l'on clique sur le coeur, le but est d'inverser l'état en cours
+    songItem.addEventListener('favorite_click', () => {
+      // Est-ce que ça nous retourne quelque chose (et donc que c'est dans la liste?)
+      if(getItem(song.id)) {
+        removeItem(song.id)
+      } else {
+        setItem(song.id, song)
+      }
+
+      // On met à jour l'attribute favorite pour déterminer son état
+      songItem.setAttribute('favorite', !!getItem(song.id))
+    })
+
+    // Insérer dans la liste
+    songList.appendChild(songItem)
+  })
+}
+
+// S'occupe d'afficher les chansons d'un artiste, selon son ID.
+// Pour cela, on va utiliser loadSongs du fichiers api.js qui lui sait nous retourner
+// un tableau de chanson, selon l'id d'un artiste
+const displayArtistSongs = async (id) => {
+  // Récupérer la liste des chansons depuis l'api
+  const songs = await loadSongs(id)
+
+  // Titre à jour
+  titreList.innerHTML = `Artistes > ${songs[0].artist.name}`
+
+  displaySongArray(songs)
+}
+
+// S'occupe d'afficher les chansons d'un artiste, selon son ID.
+// Pour cela, on va utiliser loadSongs du fichiers api.js qui lui sait nous retourner
+// un tableau de chanson, selon l'id d'un artiste
+const displaySearchSongs = async (query) => {
+  // Récupérer la liste des chansons depuis l'api
+  const songs = await loadSearch(query)
+
+  // Titre à jour
+  titreList.innerHTML = `Résultats de recherche pour : ${decodeURIComponent(query)}`
+
+  displaySongArray(songs)
+}
+
+// S'occupe d'afficher les chansons favorites
+// Pour cela, on va utiliser getItems du fichier local-storage.js qui lui sait nous retourner
+// un tableau de chanson, stocké dans localStorage
+const displayFavoriteSongs = () => {
+  const songs = getItems()
+
+  titreList.innerHTML = `Favoris`
+  displaySongArray(songs)
+}
+
+export { displayArtistSongs, displaySearchSongs, displayFavoriteSongs }
